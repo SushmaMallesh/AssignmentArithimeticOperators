2087468c45e07b50dff8668588b3eb163f91dbcc
SushmaMallesh <87855927+SushmaMallesh@users.noreply.github.com>
2021-07-23 16:10:15 +0530

    Add files via upload

---
+++ ArithimeticOperatorsDemo.java
@@ -0,0 +1,17 @@
+class ArithimeticOperatorsDemo{
+int a=22;
+int b=55;
+int c,d,e,f;
+public static void main(String args[])
+{
+ArithimeticOperatorsDemo aod=new ArithimeticOperatorsDemo();
+aod.c=aod.a+aod.b;
+System.out.println("addition of 2 numbers are="+aod.c);
+aod.d=aod.a-aod.b;
+System.out.println("subtraction of 2 numbers are="+aod.d);
+aod.e=aod.a*aod.b;
+System.out.println("multiplication of 2 numbers are="+aod.e);
+aod.f=aod.a%aod.b;
+System.out.println("modulus of 2 numbers are="+aod.f);
+}
+}
